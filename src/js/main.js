7c28c11a2d76296a0582575dcda0b9ad8112204d
Lexx <alieksiei.nikonov.87@mail.ru>
2022-03-09 22:42:10 +0300

    2 Added modal windows

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import "./slider";
+import modals from "./moduls/modals";
+
+window.addEventListener("DOMContentLoaded", () => {
+  modals();
+});
---
+++ src/js/moduls/modals.js
@@ -0,0 +1,47 @@
+const modals = () => {
+  function showModalWindow(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
+
+    trigger.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+      });
+    });
+
+    close.addEventListener("click", () => {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+    });
+
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+      }
+    });
+  }
+
+  function showModalWindowByTime(selector, time) {
+    setTimeout(function () {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "hidden";
+    }, time);
+  }
+
+  showModalWindow(
+    ".popup_engineer_btn",
+    ".popup_engineer",
+    ".popup_engineer .popup_close"
+  );
+  showModalWindow(".phone_link", ".popup", ".popup .popup_close");
+  showModalWindowByTime(".popup", 60000);
+};
+
+export default modals;
